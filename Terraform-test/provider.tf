0958c6cc1c71f5a3ae81b59c8ed1586672a0c966
Ahmedfathyy <100867143+Ahmedfathyy@users.noreply.github.com>
2023-02-02 13:29:21 +0200

    Update provider.tf

--- Terraform-test/provider.tf
+++ Terraform-test/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIA6MTQTU7UHJH4TF4Y"
-  secret_key = "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"
+  access_key = "ACCESS KEY"
+  secret_key = "SECRET KEY"
   region     = "us-east-1"
 }
